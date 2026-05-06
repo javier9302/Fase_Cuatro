a61dc58770962a3000c4fb4d59a6604647bc8bee
Javier9302 <javier9302@gmail.com>
2026-05-06 18:04:08 +0900

    Setters y getters sin validación

--- cliente.py
+++ cliente.py
@@ -6,3 +6,25 @@ class Cliente:
         self._id=id
         self._email=email
 
+    #Creación de setters para los principales atributos 
+    def set_nombre(self,nombre):
+        self._nombre=nombre
+
+    def set_id(self,id):
+        self._id=id
+
+    def set_email(self,email):
+        self._email=email
+
+    
+    #Creacion de getters
+
+    def get_nombre(self):
+        return self._nombre
+    
+    def get_id(self):
+        return self._id
+    
+    def get_email(self):
+        return self._email
+    
