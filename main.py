aa91cc3103a58515a0d2e4744ce58075d95c874c
Javier9302 <javier9302@gmail.com>
2026-05-06 23:23:49 +0900

    Se agregan comentarios, try/except para que el sistema continue cuando encuentre un error

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 #Importar la clase Cliente del archivo cliente para usarla en el programa
 from cliente import Cliente
-
+from excepciones import ErrorValidacion
 clientes=[]
+
+#*****IMPORTANTE*******
+#La siguiente sección se puede eliminar, son solamente pruebas del módulo clientes
+#Los encargados de Main y de logs lo pueden usar para realizar pruebas
+
 
 #Pruebas para crear listas de clientes y probar errores comunes.
 
 #Datos correctos
+try: 
+    clientes.append(Cliente("Pedro",10123456,"pedro@sucorreo.com"))
+except ErrorValidacion as e:
+    print("Error:", e)
 
-clientes.append(Cliente("Pedro",10123456,"pedro@sucorreo.com"))
 
 #Campo nombre vacío
-clientes.append(Cliente("",10123.457,"pedro2@sucorreo.com"))
+try: 
+    clientes.append(Cliente("",10123.457,"pedro2@sucorreo.com"))
+except ErrorValidacion as e:
+    print("Error:", e)
 
 #Campo correo no cumple con requisitos
-clientes.append(Cliente("Pedro",10123458,"pedro3sucorreo.com"))
-
+try:
+    clientes.append(Cliente("Pedro",10123458,"pedro3sucorreo.com"))
+except ErrorValidacion as e:
+    print("Error:", e)
 
 print(clientes[0])
+
+#Hasta acá se puede eliminar
+#Termina pruebas de cliente
