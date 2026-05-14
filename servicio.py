da77584a1ea0e7d192039bfe29e766f71787c6d7
Javier9302 <javier9302@gmail.com>
2026-05-14 09:52:55 +0900

    Agregadas subclases de Equipo, Asesoria y Salas

--- servicio.py
+++ servicio.py
@@ -1,17 +1,32 @@
 class Servicio:
-    def __init__(self, nombre, costo, disponibilidad,estado,id, descripcion):
+    def __init__(self, nombre, costo, disponibilidad,estado,id, tipo, descripcion):
         self.nombre = nombre
         self.costo = costo
         self.disponibilidad = disponibilidad
         self.estado = estado
         self.id = id
+        self.tipo = tipo
         self.descripcion = descripcion
+        
 
 class Sala(Servicio):
-    def __init__(self,nombre,costo, disponibilidad,estado,id, descripcion, capacidad,proyector,tipo):
-        super().__init__(nombre, costo, disponibilidad,estado,id, descripcion)
+    def __init__(self,nombre,costo, disponibilidad,estado,id,tipo, descripcion, capacidad,proyector):
+        super().__init__(nombre, costo, disponibilidad,estado,id,tipo, descripcion)
         self.capacidad = capacidad
         self.proyector = proyector
-        self.tipo = tipo
+        
 
-class 
+class Equipo(Servicio):
+    def __init__(self,nombre,costo, disponibilidad,estado,id, descripcion, tipo, marca, serial, cantidad,):
+        super().__init__(nombre, costo, disponibilidad,estado,id, descripcion, tipo )    
+        self.marca=marca
+        self.serial=serial
+        self.cantidad=cantidad
+        
+
+class Asesoria(Servicio):
+    def __init__(self, nombre,costo, disponibilidad,estado,id, descripcion, tipo, modalidad,duracion,asesor):
+        super().__init__(nombre, costo, disponibilidad,estado,id, descripcion, tipo)
+        self.modalidad = modalidad
+        self.duracion = duracion
+        self.asesor = asesor
